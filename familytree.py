64f5dd289f7dd663bcd4a602caec898714881fef
Sam Guyer <sam.guyer@gmail.com>
2020-11-11 22:53:40 -0500

    Modified to read data file instead of hard-coding family tree

--- familytree.py
+++ familytree.py
@@ -1,78 +1,96 @@
 
-# A person is a tuple of a name and two parents
-
-p0 = ('unknown', None, None)
-p1 = ('Samuel', p0, p0)
-p2 = ('Nora', p0, p0)
-p3 = ('Isabel', p1, p2)
-p4 = ('James', p0, p0)
-p5 = ('Ethel', p0, p0)
-p6 = ('Jim', p4, p5)
-p7 = ('Jane', p6, p3)
-p8 = ('Peter', p6, p3)
-p9 = ('Liza', p6, p3)
-p10 = ('Tim', p6, p3)
-p11 = ('Manny', p0, p0)
-p12 = ('Elsa', p0, p0)
-p13 = ('Channa', p11, p12)
-p14 = ('Irving', p0, p0)
-p15 = ('Ida', p0, p0)
-p16 = ('Sydney', p14, p15)
-p17 = ('Bernard', p16, p13)
-p18 = ('Marilyn', p16, p13)
-p19 = ('Evelyn', p16, p13)
-p20 = ('Sam', p17, p7)
-p21 = ('Nathan', p17, p7)
-p22 = ('Kate', p17, p7)
-p23 = ('Sid', p0, p0)
-p24 = ('Adrianna', p0, p0)
-p25 = ('Ellyn', p23, p24)
-p26 = ('Bert', p0, p0)
-p27 = ('Marcy', p0, p0)
-p28 = ('Stephen', p26, p27)
-p29 = ('Aliza', p28, p25)
-p30 = ('David', p28, p25)
-p31 = ('Michael', p28, p25)
-p32 = ('Jonah', p20, p29)
-p33 = ('Nella', p0, p0)
-p34 = ('Jacob', p30, p33)
-p35 = ('Jonathan', p30, p33)
-p36 = ('Hanna', p30, p33)
-p37 = ('Amanda', p0, p0)
-p38 = ('Hannah', p21, p37)
-p39 = ('Owen', p21, p37)
-p40 = ('Bill', p0, p0)
-p41 = ('William', p40, p22)
-p42 = ('Grace', p40, p22)
-
-everyone = [p1, p2, p3, p4, p5, p6, p7, p8, p9, p10,
-            p11, p12, p13, p14, p15, p16, p17, p18, p19, p20,
-            p21, p22, p23, p24, p25, p26, p27, p28, p29, p30,
-            p31, p32, p33, p34, p35, p36, p37, p38, p39, p40, p41, p42]
-
-def lookup(name, people):
+# -- Find a person by name
+#    Given a list of people (list of tuples of form name, parent1, parent2)
+#    Find the person with the given name, or return None
+def find_person(name, people):
     for p in people:
-        if name == p[0]:
+        if p[0] == name:
             return p
     return None
 
-def is_parent(parent, child):
+# -- Read family tree file
+#    The file is a list of lines, one for each person in the family.
+#    The first word on the line is the person's name; the second and
+#    third names on the line are the person's parents (possibly unknown)
+#    NOTE: We assume that the file is ordered so that a person's parents
+#          are on earlier lines, so that they are already there when
+#          we look them up.
+def read_family_file(familyfile):
+    people = []
+    with open(familyfile) as f:
+        for line in f:
+            names = line.rstrip().split(' ')
+            parent1 = find_person(names[1], people)
+            parent2 = find_person(names[2], people)
+            person = (names[0], parent1, parent2)
+            people.append(person)
+    return people
+
+# -- Return the father of p
+def father_of(p):
+    return p[1]
+
+# -- Return the mother of p
+def mother_of(p):
+    return p[2]
+
+# ---- Coding problems here ----------------------------------------
+
+# -- Return True if p1 is the mother of p2
+def is_mother(p1, p2):
     return False
 
-def are_siblings(child1, child2):
+# -- Return True if p1 is the parent of p2
+def is_parent(p1, p2):
     return False
 
-def are_cousins(c1, c2):
+# -- Return True if p1 and p2 are siblings
+def is_sibling(p1, p2):
     return False
 
+# -- Return True if p1 and p2 are cousins
+#    Try Jonah and Owen (True), David and Aliza (False), and
+#    Ellyn and Jane (False, should not cause an error)
+def is_cousin(p1, p2):
+    return False
 
-def all_children(parent, people):
-    return
+# -- Return True is p1 is the maternal grandmother of p2
+#    Try Jane and Jonah (True), Jane and Jacob (False), and
+#    Jane and Ethel (False, should not cause error)
+def is_maternal_grandmother(p1, p2):
+    return False
 
-def all_cousins(person, people):
-    return
+# -- Return True is p1 is the maternal great grandmother of p2
+#    Try Isabel and Jonah (True)
+def is_maternal_great_grandmother(p1, p2):
+    return False
 
+# -- Return True is p1 is the maternal great great grandmother of p2
+#    Try Nora and Jonah
+def is_maternal_great_great_grandmother(p1, p2):
+    return False
 
-# ---- Program starts here ---------------------------
+# -- Return True is p1 is a maternal ancestor of p2
+#    That is, if p1 is p2's mother, or p2's mother's mother, etc.
+def is_maternal_ancestor(p1, p2):
+    return False
 
+# -- Return True if p1 is an ancestor of p2
+#    On either side of the family tree
+def is_ancestor(p1, p2):
+    return False
 
+# ---- Main program ------------------------------------------------
+
+fn = input('Enter family tree file name: ')
+people = read_family_file(fn)
+print('Found {} people'.format(len(people)))
+
+s = find_person('Sam', people)
+j = find_person('Jane', people)
+if is_mother(j, s):
+    print('Yes')
+else:
+    print('No')
+
+# --- Add more test cases here ------
