e5389cc70db463cfc6024afed372cc4e65a9762a
Sam Guyer <sam.guyer@gmail.com>
2020-11-12 08:25:46 -0500

    Moved exercises into separate document

--- warmup.py
+++ warmup.py
@@ -6,21 +6,6 @@
 # functionC calls functionD
 # functionD calls functionE
 #
-# EXERCISE 1:
-# Run the program and look at the output. What is the sequence of
-# statements that happens? Why do the numbers go up and then go
-# back down? What does the value of "d" represent? Think about
-# how the second test (starting at functionB) differs from the first.
-
-# EXERCISE 2:
-# Modify these functions so that no matter where we start, it only
-# goes three levels deep (that is, stop when d is 3).
-
-# EXERCISE 3:
-# Notice that the functions are almost identical. Do we really need
-# all of these identical functions? What happens if change functionA
-# so that instead of calling functionB, it calls ITSELF. Try it.
-# A function that calls itself is called a RECURSIVE function.
 
 def functionE(d):
     print('Start function E value is ' + str(d))
--- warmup2.py
+++ warmup2.py
@@ -3,48 +3,24 @@
 # pass a list down and each function prints one element
 # from the list
 
-# EXERCISE 1
-# Run the program and make sure you understand the sequence
-# of statements that happens.
-
-# EXERCISE 2
-# What is the value of the variable 'd' in each function?
-# Can we replace the index number with d? Try it.
-
-# EXERCISE 3
-# What happens if the list has fewer than four elements? Try
-# it. Change the functions so that they check to make sure
-# that the list is long enough for each index operation. That
-# is, compare 'd' against the length of the list. Try it with
-# lists of size 2, 3, and 4.
-
-# EXERCISE 4
-# Once again, we have a set of functions that all look almost
-# identical. What happens if we make print_starting_at_0 call
-# itself instead of calling print_starting_at_1? In other words
-# make it a RECURSIVE function. Try it on lists of a bunch of
-# different sized lists.
-
-# CHALLENGE PROBLEM
-# Can you modify the print_starting_at_0 function so that it
-# prints the list in REVERSE order? Think about the output
-# from the first set of warmup exercises -- which statements
-# appeared in reverse order?
-
+# -- Print element 3 of a list
 def print_starting_at_3(d, thelist):
     v = thelist[3]
     print(v)
 
+# -- Print element 2, then call function to print 3
 def print_starting_at_2(d, thelist):
     v = thelist[2]
     print(v)
     print_starting_at_3(d+1, thelist)
 
+# -- Print element 1, then call function to print 2 and 3
 def print_starting_at_1(d, thelist):
     v = thelist[1]
     print(v)
     print_starting_at_2(d+1, thelist)
 
+# -- Print element 0, then call function to print 1,2,3
 def print_starting_at_0(d, thelist):
     v = thelist[0]
     print(v)
